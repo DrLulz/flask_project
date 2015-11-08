68c6134469567bbc49c55bf19c32e798b0361a2b
DrLulz <drlulzmd@gmail.com>
2015-11-08 15:38:41 -0500

    datetime

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,13 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField, DateField, IntegerField
 from wtforms.validators import DataRequired
+from datetime import date
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
     
 class TaperForm(Form):
-    start_date = DateField('Start Date', format='%m/%d/%Y', default=datetime.today, validators=[DataRequired()])
+    start_date = DateField('Start Date', format='%m/%d/%Y', default=date.today, validators=[DataRequired()])
     duration = IntegerField('Duration', validators=[DataRequired()])
     pill_size = IntegerField('Dose', validators=[DataRequired()])
