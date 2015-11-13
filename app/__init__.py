4f295d8cb8bdab2fb0c3a444a49762c0ecda65de
DrLulz <drlulzmd@gmail.com>
2015-11-13 17:01:37 -0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -5,4 +6,21 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+from app import views, models
+"""
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+# Fixes circular import
 from app import views, models
--- run.py
+++ run.py
@@ -1,2 +1,4 @@
-from app import app
-app.run(debug=True)
+#from app import app
+#app.run(debug=True)
+from app import manager
+manager.run()
